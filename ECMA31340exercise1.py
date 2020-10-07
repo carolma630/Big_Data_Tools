13a9faa00694e18995f2116d41fd527d8105ca54
Xiaoyuan Ma <xiaoyuanma@XiaoyuandeMacBook-Pro.local>
2020-10-07 20:43:17 +0800

    Did some revision

--- ECMA31340exercise1.py
+++ ECMA31340exercise1.py
@@ -1,7 +1,3 @@
-
-#Find all Backwards Read Primes between two positive given numbers (both inclusive), the second one always being greater than or equal to the first one.
-# The resulting array or the resulting string will be ordered following the natural order of the prime numbers.
-
 def backwardsPrime(lb, ub):
     lst=[]
 
@@ -17,28 +13,15 @@ def backwardsPrime(lb, ub):
 
         #if x is a prime number, determine whether the backward of x is not equal to x, if no, determine whether it is a prime number
         if bkwdprime:
-            digits = len(str(x))
-            bkwd_x = 0
-            for i in range(0, digits):
-                bkwd_x = bkwd_x + int(str(x)[i]) * 10 ** i
+            bkwd_x=int(str(x)[::-1])
 
             if bkwd_x!=x:
                 for m in range(2, bkwd_x):
                     if bkwd_x % m == 0:
                         bkwdprime = False
                         break
-
-            else:
-                bkwdprime=False
-
-        if bkwdprime:
-            lst.append(x)
-
+                if bkwdprime:
+                    lst.append(x)
 
     return lst
 
-#test function
-
-prime=backwardsPrime(9900,10000)
-
-print(prime)
